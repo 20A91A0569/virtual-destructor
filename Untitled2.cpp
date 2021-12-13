5232c93da1186dce3dd6dbe7f83a0ac1343a14d5
20A91A0569 <84494697+20A91A0569@users.noreply.github.com>
2021-12-13 21:10:39 +0530

    Add files via upload

---
+++ Untitled2.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+class base
+{
+	public:
+		base()
+		{
+			cout<<"base class constructor"<<endl;
+		}
+		virtual ~base()
+		{
+			cout<<"base class destructor"<<endl;
+		}
+};
+class derived:public base
+{
+	public:
+		derived()
+		{
+			cout<<"derived class constructor"<<endl;
+		}
+		~derived()
+		{
+			cout<<"derived class destructor"<<endl;
+		}
+};
+int main()
+{
+	base *bptr;
+	derived d;
+	
+	delete bptr;
+	return 0;
+}
